2ecde19efdda342c675b4579fc9a801010cbf335
ymh <suneverset@naver.com>
2021-04-23 01:07:05 +0900

    210423 wrong code fixed

--- 210417_모험가길드.py
+++ 210417_모험가길드.py
@@ -8,7 +8,7 @@ group = []
 for _ in range(len(horror_list)):
     horror = horror_list.pop(0)
     group.append(horror)
-    if len(group) < max(group):
+    if len(group) < max(group): # 그룹의 인원수와 그룹 내 max 공포도를 비교
         continue
     else:
         cnt += 1
