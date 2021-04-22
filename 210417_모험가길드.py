090a2c7bfd089564729d7f0124760ee37f998db5
ymh <suneverset@naver.com>
2021-04-23 01:06:28 +0900

    210423 wrong code fixed

--- 210417_모험가길드.py
+++ 210417_모험가길드.py
@@ -4,10 +4,13 @@ num = int(input('모험가는 몇 명?'))
 horror_list = sorted(list(map(int, input('공포도를 나열하세용.').split())))
 guild_sum = 0 # (앞으로 더해질) 모험가의 수
 cnt = 0
-while guild_sum < len(horror_list):
-    horror = horror_list[guild_sum]
-    if max(horror_list[guild_sum:guild_sum + horror]) <= horror:
+group = []
+for _ in range(len(horror_list)):
+    horror = horror_list.pop(0)
+    group.append(horror)
+    if len(group) < max(group):
+        continue
+    else:
         cnt += 1
-    guild_sum += horror
-
+        group = []
 print(cnt)
