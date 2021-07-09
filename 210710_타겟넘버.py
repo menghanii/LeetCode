9caf7947ed45b2e7951c983ad34d238cb0c28f62
ymh <suneverset@naver.com>
2021-07-10 02:30:39 +0900

    Programmers : 타겟넘버 - 미완성

---
+++ 210710_타겟넘버.py
@@ -0,0 +1,12 @@
+def solution(numbers, target):
+    size = len(numbers)
+    answer = 0
+    
+    cnt = 0
+    arr = []
+    for n in range(len(numbers)):
+        for i in [1, -1]:
+            arr.append(n * i)
+    return answer
+
+solution([1,1,1,1,1], 5)
