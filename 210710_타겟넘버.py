76ac76a2d645dfec7774c4d787a1ac4b9ca5501a
menghanii <37925813+menghanii@users.noreply.github.com>
2021-07-19 23:56:42 +0900

    Update 210710_타겟넘버.py

--- 210710_타겟넘버.py
+++ 210710_타겟넘버.py
@@ -1,13 +1,24 @@
-#210716 still not resolved
-def solution(numbers, target):
-    size = len(numbers)
-    answer = 0
-    
-    cnt = 0
-    arr = []
-    for n in range(len(numbers)):
-        for i in [1, -1]:
-            arr.append(n * i)
-    return answer
+from itertools import combinations
+# def solution(numbers, target): # numbers = [1,2,3,4,5]
+#     for i in range(len(numbers)):
+#     answer = 0
+#     sum_list = [sum(numbers)]
+#     for i in range(len(numbers)):
+#         plus = numbers[:len(numbers)-(i+1)]
+#         minus = [-j for j in numbers[len(numbers)-(i+1):]] # [-3, -4, -5]
+#         print(plus)
+#         print(minus)
+#         while minus:
+#             print(sum(plus) + sum(minus))
+#             sum_list.append(sum(plus) + sum(minus))        
+#             plus.append(minus.pop() * -1)
+#         print()
+#     return sum_list
 
-solution([1,1,1,1,1], 5)
+# print(solution([1,2,3,4,5], 3))
+numbers = [1,2,3,4,5]
+for i in range(1, len(numbers)+1):
+    combi = combinations([0,1,2,3,4], i)
+    for c in combi:
+        for i in range(len(c)):
+            print(-1 * numbers[c[i]])
